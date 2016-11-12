b9467c8a2ff1498d0773faee9aee672f32e65751
hardog <peng.zhao@husor.com.cn>
2016-11-12 18:31:02 +0800

    add files

--- example/basic.js
+++ example/basic.js
@@ -1,9 +1,7 @@
 var Fire = require('../lib/log');
 var f = new Fire({
     host: '192.168.40.36',
-    port: 9998,
-    tag: 'Basic',
-    terminal: 'on' // this is default
+    port: 9998
 });
 
 f.log('hello, this is log');
